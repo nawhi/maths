4b4e4575274ff35253a9bd520e2e9b6895f1afe7
Nick White <nickw@filmlight.ltd.uk>
2018-07-27 14:58:58 +0100

    Add some basic tests for LU-decomposition

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -23,7 +23,7 @@ Matrix<int> identity()
     };
 }
 
-TEST_CASE( "Matrices can be compared for equality")
+TEST_CASE( "Matrices can be compared for equality" )
 {
     auto i = identity();
     auto i2 = identity();
@@ -32,7 +32,7 @@ TEST_CASE( "Matrices can be compared for equality")
     REQUIRE( i != zero() );
 }
 
-TEST_CASE( "Matrices can be multiplied together")
+TEST_CASE( "Matrices can be multiplied together" )
 {
     auto z = zero();
     REQUIRE( z*z == z );
@@ -40,4 +40,18 @@ TEST_CASE( "Matrices can be multiplied together")
     auto i = identity();
     REQUIRE( i*i == i );
 
+}
+
+TEST_CASE( "LU decomposition of I and 0 works" )
+{
+    auto i = identity();
+    REQUIRE( lu_decomp(i) == std::make_pair(i, i) );
+
+    auto z = zero();
+    REQUIRE( lu_decomp(i) == std::make_pair(z, z) );
+}
+
+TEST_CASE( "LU decomposition of simple integer matrix works" )
+{
+    // todo
 }
