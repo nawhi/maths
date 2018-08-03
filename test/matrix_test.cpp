7d61b386701ad4fb30ed1f44ade8f71c52e9a988
Nick White <nickw@filmlight.ltd.uk>
2018-08-03 12:33:54 +0100

    Fix error in LU-decomposition tests

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -141,5 +141,5 @@ TEST_CASE( "LU decomposition of simple integer matrix works" )
     std::cout << res.first << std::endl;
     std::cout << res.second << std::endl;
     REQUIRE( is_upper_diag(res.second) );
-    REQUIRE( res.first * res.second == identity );
+    REQUIRE( res.first * res.second == decomp_input );
 }
