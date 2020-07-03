888cfbf147eb588013a302c8704ee7557e515d86
nawhi <25394965+nawhi@users.noreply.github.com>
2020-07-04 00:03:52 +0100

    Finish implementing Euclid gcd

--- include/rational.h
+++ include/rational.h
@@ -1,16 +1,25 @@
 #ifndef MATRIX_RATIONALS_H
 #define MATRIX_RATIONALS_H
+#include <iostream>
 
+template<typename I>
+I gcd(I a, I b) {
+    std::cout << "gcd(" << a << "," << b << ")" << std::endl;
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
 
-template <typename I>
+template<typename I>
 class Rational {
 private:
     I num;
     I denom;
 public:
-    explicit Rational(const I&& n): num(n), denom(1) {}
+    explicit Rational(const I &&n) : num(n), denom(1) {}
 
-    bool operator == (const Rational& other) const {
+    Rational(const I &&n, const I &&d) : num(n), denom(1) {}
+
+    bool operator==(const Rational &other) const {
         return num == other.num && denom == other.denom;
     }
 };
--- test/rational_test.cpp
+++ test/rational_test.cpp
@@ -9,7 +9,23 @@
 // gt, ge, lt, le
 // ostream
 
-TEST_CASE("Rational class construction", "[rational]") {
-    CHECK(Rational<int>(1) == Rational<int>(1));
+TEST_CASE("gcd (Euclid)", "[gcd]") {
+    CHECK(gcd<int>(1, 0) == 1);
+    CHECK(gcd<int>(1, 1) == 1);
+    CHECK(gcd<int>(2, 2) == 2);
+    CHECK(gcd<int>(6, 3) == 3);
+    CHECK(gcd<int>(8, 12) == 4);
+    CHECK(gcd<int>(323, 437) == 19);
+}
+
+TEST_CASE("Rational class equality", "[rational]") {
+    SECTION("trivial examples") {
+        CHECK(Rational<int>(1) == Rational<int>(1));
+        CHECK(Rational<int>(1, 2) == Rational<int>(1, 2));
+    }
+
+    SECTION("multiples") {
+        CHECK(Rational<int>(2, 4) == Rational<int>(1, 2));
+    }
 }
 
