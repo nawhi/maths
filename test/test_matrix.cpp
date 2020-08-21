b1d7cde4cc5d969b91be83334c23bb9946ad2f90
nawhi <25394965+nawhi@users.noreply.github.com>
2020-08-21 17:37:40 +0100

    One more test for Matrix streaming

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -10,11 +10,18 @@ TEST_CASE("Matrix construction", "[matrix]") {
 }
 
 TEST_CASE("Matrix streaming", "[matrix]") {
-    Matrix<int> mx({{1, 2},
-                    {3, 4}});
+    Matrix<int> mx = {{1, 2},
+                      {3, 4}};
 
     const auto s = (std::ostringstream() << mx).str();
     CHECK(s == "[1 2]\n[3 4]");
+
+    Matrix<double> mx2 = {{1, 2, 3},
+                          {4, 5, 6},
+                          {7, 8, 9}};
+
+    const auto s2 = (std::ostringstream() << mx2).str();
+    CHECK(s2 == "[1 2 3]\n[4 5 6]\n[7 8 9]");
 
 }
 
